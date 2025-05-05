d33ca0680881a451aad60127d8a2dc3fff9556f6
Louis <2720447678@qq.com>
2025-05-06 01:35:35 +0800

    其它

--- app.js
+++ app.js
@@ -5,23 +5,67 @@ const port = 3917
 const app = express()
 
 app.use('/consumer', (req, res)=>{
-
   const filepath = '/var/www/shopping/shopping-consumer'
-
   exec(`git -C ${filepath} pull`, (error, stdout, stderr)=>{
     if (error) {
-      console.error(`执行 git pull 时出错: ${error}`);
+      console.error(`consumer 执行 git pull 时出错: ${error}`);
       return res.status(500).send(`执行 git pull 时出错: ${error.message}`);
     }
     if (stderr) {
-      console.error(`执行 git pull 时的错误输出: ${stderr}`);
+      console.error(`consumer 执行 git pull 时的错误输出: ${stderr}`);
       return res.status(500).send(`执行 git pull 时的错误输出: ${stderr}`);
     }
-    console.log(`git pull 执行成功: ${stdout}`);
+    console.log(`consumer git pull 执行成功: ${stdout}`);
     res.send(`git pull 执行成功: ${stdout}`);
   })
-  
+})
 
+app.use('/seller', (req, res)=>{
+  const filepath = '/var/www/shopping/shopping-seller-pc'
+  exec(`git -C ${filepath} pull`, (error, stdout, stderr)=>{
+    if (error) {
+      console.error(`seller 执行 git pull 时出错: ${error}`);
+      return res.status(500).send(`执行 git pull 时出错: ${error.message}`);
+    }
+    if (stderr) {
+      console.error(`seller 执行 git pull 时的错误输出: ${stderr}`);
+      return res.status(500).send(`执行 git pull 时的错误输出: ${stderr}`);
+    }
+    console.log(`seller git pull 执行成功: ${stdout}`);
+    res.send(`git pull 执行成功: ${stdout}`);
+  })
+})
+
+app.use('/admin', (req, res)=>{
+  const filepath = '/var/www/shopping/shopping-admin-pc'
+  exec(`git -C ${filepath} pull`, (error, stdout, stderr)=>{
+    if (error) {
+      console.error(`admin 执行 git pull 时出错: ${error}`);
+      return res.status(500).send(`执行 git pull 时出错: ${error.message}`);
+    }
+    if (stderr) {
+      console.error(`admin 执行 git pull 时的错误输出: ${stderr}`);
+      return res.status(500).send(`admin 执行 git pull 时的错误输出: ${stderr}`);
+    }
+    console.log(`admin git pull 执行成功: ${stdout}`);
+    res.send(`git pull 执行成功: ${stdout}`);
+  })
+})
+
+app.use('/autoPull', (req, res)=>{
+  const filepath = '/var/www/shopping/autoPull'
+  exec(`git -C ${filepath} pull`, (error, stdout, stderr)=>{
+    if (error) {
+      console.error(`autoPull 执行 git pull 时出错: ${error}`);
+      return res.status(500).send(`执行 git pull 时出错: ${error.message}`);
+    }
+    if (stderr) {
+      console.error(`autoPull 执行 git pull 时的错误输出: ${stderr}`);
+      return res.status(500).send(`执行 git pull 时的错误输出: ${stderr}`);
+    }
+    console.log(`autoPull git pull 执行成功: ${stdout}`);
+    res.send(`git pull 执行成功: ${stdout}`);
+  })
 })
 
 app.listen(port, () => {
